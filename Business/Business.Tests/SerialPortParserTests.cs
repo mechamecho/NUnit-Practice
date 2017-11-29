baeaafffbee7eb03d984436482e4ddb2a2fa81e3
Livepurposefully <nafissa_92@hotmail.com>
2017-11-28 23:54:24 -0500

    Initial commit 'Unit Test 1'

---
+++ Business/Business.Tests/SerialPortParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Business.Tests
+{
+    [TestFixture]
+    class SerialPortParserTests
+    {   
+        [Test]
+        public void ParsePort_COM1_Returns1()
+        {
+            int result = SerialPortParser.ParsePort("COM1");
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
